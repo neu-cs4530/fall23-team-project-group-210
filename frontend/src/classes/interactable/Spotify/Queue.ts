a8d9c570de5883f719d024674dbcaf8da327ed9b
Michael Delaney <michaeldelaney@Michaels-Laptop.local>
2023-10-31 18:05:23 -0400

    initial commit of spotifyArea backend. Unfinished details, but set up initial methods and structure. If there is anything missing from the methods in spotifyAreaController or the signatures could be better, leavea comment.

---
+++ frontend/src/classes/interactable/Spotify/Queue.ts
@@ -0,0 +1,24 @@
+import { Song } from './SpotifyAreaController';
+
+/**
+ * Basic generic Queue class. Need to update to allow queue order to be changed by votes.
+ */
+export class SongQueue {
+  private _storage: Song[] = [];
+
+  enqueue(song: Song): void {
+    this._storage.push(song);
+  }
+
+  dequeue(): Song | undefined {
+    return this._storage.shift();
+  }
+
+  orderByLikes(): void {
+    this._storage.sort((a: Song, b: Song) => b.likes - a.likes);
+  }
+
+  size(): number {
+    return this._storage.length;
+  }
+}
